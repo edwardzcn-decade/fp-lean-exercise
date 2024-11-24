7c7df730f64b6f8235b7658c09314f7e580f6a5a
edwardzcn-decade <edwardzcn98@gmail.com>
2024-11-24 19:24:43 +0800

    feat: update ch5

--- Ch5/Class.lean
+++ Ch5/Class.lean
@@ -1,1 +1,15 @@
 -- Monad Class Type
+
+-- And then (like in the Rust)
+def andThen (option : Option α) (next : α → Option β) :=
+  match option with
+  | none => none
+  | some value => next value
+
+def getThirdFourth : List α → Option (α × α) :=
+  fun list =>
+  andThen list[2]? fun third =>
+  andThen list[3]? fun fourth =>
+  some (third, fourth)
+
+#eval getThirdFourth [5, 4, 3, 2, 1] -- some (3, 4)
