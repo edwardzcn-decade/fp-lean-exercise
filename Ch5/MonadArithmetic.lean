e13cc3079b027322e476fdcba8ba9e3344b03762
edwardzcn-decade <edwardzcn98@gmail.com>
2024-11-28 22:28:41 +0800

    feat: update ch5.2 and give proof of monad many

--- Ch5/MonadArithmetic.lean
+++ Ch5/MonadArithmetic.lean
@@ -101,21 +101,21 @@ def evaluateExcept : Expr Arith → Except String Int
 
 
 -- Try Monad
-def evalueteMonad [Monad m] (applyPrimU: Arith → Int → m Int) (applyPrimO: Arith → Int → Int → m Int): Expr Arith → m Int
+def evaluateMonad [Monad m] (applyPrimU: Arith → Int → m Int) (applyPrimO: Arith → Int → Int → m Int): Expr Arith → m Int
   | Expr.const i => pure i
   | Expr.unode p e1 =>
-    evalueteMonad applyPrimU applyPrimO e1 >>= fun v1 =>
+    evaluateMonad applyPrimU applyPrimO e1 >>= fun v1 =>
     applyPrimU p v1
   | Expr.prim p e1 e2 =>
-    evalueteMonad applyPrimU applyPrimO e1 >>= fun v1 =>
-    evalueteMonad applyPrimU applyPrimO e2 >>= fun v2 =>
+    evaluateMonad applyPrimU applyPrimO e1 >>= fun v1 =>
+    evaluateMonad applyPrimU applyPrimO e2 >>= fun v2 =>
     applyPrimO p v1 v2
 
-#eval evalueteMonad applyPrimOnUnOpOption applyPrimOnBiOpOption twoPlusThree -- some 5
-#eval evalueteMonad applyPrimOnUnOp applyPrimOnBiOp twoPlusThree -- Except.ok 5
+#eval evaluateMonad applyPrimOnUnOpOption applyPrimOnBiOpOption twoPlusThree -- some 5
+#eval evaluateMonad applyPrimOnUnOp applyPrimOnBiOp twoPlusThree -- Except.ok 5
 
 -- now continue to compose redundant code
--- define polymorphic applyPrimeU
+-- define polymorphic applyPrimU
 
 def applyDivOption (x : Int) (y : Int) : Option Int :=
     if y == 0 then
@@ -134,7 +134,7 @@ def applyPrimO [Monad m] (applyDiv : Int → Int → m Int) : Arith → Int → 
   | Arith.times, x, y => pure (x * y)
   | Arith.div, x, y => applyDiv x y
 
-def applyPrimeU [Monad m] (applyDiv : Int → Int → m Int) : Arith → Int → m Int
+def applyPrimU [Monad m] (applyDiv : Int → Int → m Int) : Arith → Int → m Int
   | Arith.plus, x => pure x
   | Arith.minus, x => pure (-x)
   | Arith.times, x => pure x
@@ -142,16 +142,159 @@ def applyPrimeU [Monad m] (applyDiv : Int → Int → m Int) : Arith → Int →
 
 -- rewrtie evaluateMonad as evaluateM
 
-#eval evalueteMonad (applyPrimeU applyDivOption) (applyPrimO applyDivOption) twoPlusThree
-#eval evalueteMonad (applyPrimeU applyDivExcept) (applyPrimO applyDivExcept) twoPlusThree
--- well... because of the name and polymorphic applyPrimeU applyPrimeO, now its longer...
+#eval evaluateMonad (applyPrimU applyDivOption) (applyPrimO applyDivOption) twoPlusThree
+#eval evaluateMonad (applyPrimU applyDivExcept) (applyPrimO applyDivExcept) twoPlusThree
+-- well... because of the name and polymorphic applyPrimU applyPrimO, now its longer...
 --
 
+-- type Prim depends on special , Prim(x)
 inductive Prim (special : Type) where
   | plus
   | minus
   | times
   | other : special → Prim special
 
+-- broaden the scope of oprations that may fail
 inductive CanFail where
   | div
+
+def divOption : CanFail → Int → Int → Option Int
+  | CanFail.div, x, y => if y == 0 then none else pure (x / y)
+
+def divExcept : CanFail → Int → Int → Except String Int
+  | CanFail.div, x, y =>
+    if y == 0 then
+      Except.error s!"Tried to divide {x} by zero"
+    else pure (x / y)
+
+def applyPrim [Monad m] (applySpecial : special → Int → Int → m Int) : Prim special → Int → Int → m Int
+  | Prim.plus, x, y => pure (x + y)
+  | Prim.minus, x, y => pure (x - y)
+  | Prim.times, x, y => pure (x * y)
+  | Prim.other op, x, y => applySpecial op x y
+
+def evaluateM [Monad m] (applySpecial : special → Int → Int → m Int): Expr (Prim special) → m Int
+  | Expr.const i => pure i
+  | Expr.unode _ _ => pure 0
+  | Expr.prim p e1 e2 =>
+    evaluateM applySpecial e1 >>= fun v1 =>
+    evaluateM applySpecial e2 >>= fun v2 =>
+    applyPrim applySpecial p v1 v2
+
+-- Empty type and no effects
+
+def applyEmpty [Monad m] (op : Empty) (_ : Int) (_ : Int) : m Int := nomatch op
+
+-- This can be used together with Id, the identity monad, to evaluate expressions that have no effects whatsoever:
+
+-- just for passing type checking
+def applyEmptyUp [Monad m] (op: Empty) (_ : Int) : m Int := nomatch op
+
+open Expr Prim in
+
+-- can not construct Empty type
+-- #check evaluateM (m := Id) applyEmpty (prim (other CanFail.div) (const 5) (const 0))
+-- application type mismatch
+--   other CanFail.div
+-- argument
+--   CanFail.div
+-- has type
+--   CanFail : Type
+-- but is expected to have type
+--   Empty : Type
+
+open Expr Prim in
+#eval evaluateM (m := Id) applyEmpty (prim plus (const 5) (const (-14)))
+
+
+-- Nondeterministic Search
+inductive Many (α : Type) where
+| none : Many α
+| more : α → (Unit → Many α) → Many α
+-- different from List.cons: α → List α → List α to stores the rest of List (evaluation at once)
+-- Many.more store a function that represents the rest of the computation (lazy evaluation)
+
+-- A single result is represented by a `more` constructor that returns no further results:
+def Many.one (x : α) : Many α := Many.more x (fun () => Many.none)
+
+
+def Many.union : Many α → Many α → Many α
+  | Many.none, ys => ys
+  | Many.more x xs, ys => Many.more x (fun () => union (xs ()) ys)
+
+def Many.fromList : List α → Many α
+  | [] => Many.none
+  | x :: xs => Many.more x (fun () => fromList xs)
+
+def Many.take : Nat → Many α → List α
+  | 0, _ => []
+  | _ + 1, Many.none => []
+  | n + 1, Many.more x xs => x :: (xs ()).take n
+
+def Many.takeAll : Many α → List α
+  | Many.none => []
+  | Many.more x xs => x :: (xs ()).takeAll
+
+def Many.bind : Many α → (α → Many β) → Many β
+  | Many.none, _ =>
+    Many.none
+  | Many.more x xs, f =>
+    (f x).union (bind (xs ()) f)
+
+
+-- Many is a monad
+-- can prove
+
+theorem noneLeftBind (f : α → Many β) : Many.bind (Many.none) f = Many.none := by rfl
+
+theorem noneRightUnion : ∀ (m : Many α), Many.union m Many.none = m := by
+  intro m
+  induction m with
+  | none => rfl
+  | more a f ih =>
+    -- one way
+    -- unfold Many.union
+    -- simp [ih]
+    simp [Many.union]
+    funext x
+    exact ih x
+
+theorem leftIdentityMany (x : α) (f : α → Many β) : Many.bind (Many.one x) f = f x := by
+  unfold Many.one
+  unfold Many.bind
+  simp
+  unfold Many.bind
+  -- exact noneRightUnion (f x) -- or exact call noneRightUnion
+  rw [noneRightUnion] -- rw the goal
+
+
+
+
+theorem rightIdentityMany (m : Many α) : Many.bind m (Many.one) = m := by
+  induction m with
+  | none => rfl
+  | more a f ih =>
+    unfold Many.bind
+    simp [ih]
+    -- from here we can use rfl to automatically solve
+    unfold Many.one
+    unfold Many.union
+    simp
+    funext x
+    unfold Many.union
+    simp
+
+theorem associativityMany (m : Many α) (f : α → Many β) (g : β → Many γ) : Many.bind (Many.bind m f) g = Many.bind m (fun x => Many.bind (f x) g) := by
+  induction m with
+  | none => rfl
+  | more a h ih =>
+    repeat rw [Many.bind]
+    rw [←ih]
+    sorry
+
+
+    -- (Many.more a h).bind fun x => (f x).bind g
+
+    -- ((f a).bind g).union ((h ()).bind fun x => (f x).bind g)
+    -- unfold Many.bind
+    -- simp
