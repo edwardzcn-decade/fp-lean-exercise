9aaba517601b22cffeef939c1dc7a9c3ee989ccc
edwardzcn-decade <edwardzcn98@gmail.com>
2024-12-06 16:54:20 +0800

    feat: update Ch6

--- Ch6/Alternative.lean
+++ Ch6/Alternative.lean
@@ -59,22 +59,12 @@ def checkCompany' (input : RawInput) : Validate (Field × String) LegacyCheckedI
   checkThat (input.birthYear == "FIRM") "birth year" "FIRM if a company" *>
     pure .company <*>
     checkName input.name
--- is equal to?
-def checkCompanyAssoc (input : RawInput) : Validate (Field × String) LegacyCheckedInput :=
-  checkThat (input.birthYear == "FIRM") "birth year" "FIRM if a company" *>
-    (pure .company <*>
-    checkName input.name)
-
--- class SeqRight' (f : Type → Type) where
---   seqRight : f α → (Unit → f β) → f β
-----------------------------------  -------------- pure .company (β → γ)
--- target :  Validate (Field × String) Unit *> Validate (Field × String) (NonEmptyString → LegacyCheckedInput)
--- theorem eqSeqRightpure [Applicative ap] (f : α → β) (g : β → γ) : ap Unit *> pure g = pure (g ∘ f) :=
---   by
---     simp [Function.comp]
---     exact Applicative.seqRightIdentity ap g
 
 
+-- f <*> pure x = pure (f x)
+-- also ?
+-- f *> pure x = pure (f x) well .. cannot prove the control flow now
+-- neither does the simplification
 
 -- one more simplification. For every `Applicative`, `pure F <*> E` is equivalent to `f <$> E`.
 
@@ -82,3 +72,63 @@ def checkCompany (input : RawInput) : Validate (Field × String) LegacyCheckedIn
   checkThat (input.birthYear == "FIRM") "birth year" "FIRM if a company" *>
     .company <$>
     checkName input.name
+
+def checkSubtype {α : Type} (v : α) (p : α → Prop) [Decidable (p v)] (err : ε) : Validate ε {x : α // p x} :=
+  if h : p v then
+    pure ⟨v, h⟩
+  else
+    .errors { head := err, tail := [] }
+
+def checkHumanBefore1970 (input : RawInput) : Validate (Field × String) LegacyCheckedInput :=
+  (checkYearIsNat input.birthYear).andThen fun y =>
+    .humanBefore1970 <$>
+      checkSubtype y (fun x => x > 999 ∧ x < 1970) ("birth year", "less than 1970") <*>
+      pure input.name
+
+def checkHumanAfter1970 (input : RawInput) : Validate (Field × String) LegacyCheckedInput :=
+  (checkYearIsNat input.birthYear).andThen fun y =>
+    .humanAfter1970 <$>
+      checkSubtype y (· > 1970) ("birth year", "greater than 1970") <*>
+      checkName input.name
+
+def checkLegacyInput (input : RawInput) : Validate (Field × String) LegacyCheckedInput :=
+  checkCompany input <|> checkHumanBefore1970 input <|> checkHumanAfter1970 input
+
+#eval checkLegacyInput ⟨"Johnny's Troll Groomers", "FIRM"⟩
+#eval checkLegacyInput ⟨"Johnny", "1963"⟩
+#eval checkLegacyInput ⟨"", "1963"⟩
+
+#check Type 1
+
+-- inductive MyList (α : Type 1) : Type where
+--   | nil : MyList α
+--   | cons : α → MyList α → MyList α
+-- error meesage : invalid universe level in constructor
+-- solve by define in Type 1 universe level
+inductive MyList' (α : Type 1) : Type 1 where
+  | nil : MyList' α
+  | cons : α → MyList' α → MyList' α
+
+
+-- in any universe level
+inductive MyList (α : Type u) : Type u where
+  | nil : MyList α
+  | cons : α → MyList α → MyList α
+deriving Repr
+
+def myListOfNumbers : MyList Nat :=
+  .cons 0 (.cons 1 .nil)
+
+def myListOfNumberType : MyList Type :=
+  .cons Nat (.cons Int .nil)
+
+#eval myListOfNumbers -- could print
+-- #eval myListOfNumberType· -- cound not print
+-- has type
+--   MyList Type
+-- but instance
+--   Lean.Eval (MyList Type)
+
+inductive Sum''' (α : Type u) (β : Type u) : Type u where
+  | inl : α → Sum''' α β
+  | inr : β → Sum''' α β
